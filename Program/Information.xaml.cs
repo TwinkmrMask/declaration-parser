510ae34f2b475b50c32a1507cb711d5d0235c431
mrMask <rebootroot13@gmail.com>
2021-03-14 18:05:23 +0300

    Added an entry to the xslx file

--- Program/Information.xaml.cs
+++ Program/Information.xaml.cs
@@ -1,43 +1,99 @@
-﻿using System.Collections.Generic;
+﻿using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Xml;
-using System;
 
 namespace WpfApp3
 {
-    /// <summary>
-    /// Логика взаимодействия для Information.xaml
-    /// </summary>
     public partial class Information : Window
     {
-        
+
         double netWeightQuantity;
         double grossWeightQuantity;
         double positions;
         List<string> transportDocumentCodes;
+        List<(string, string)> awb;
+        public Information(string path)
+        {
+            try
+            {
+                this.grossWeightQuantity = 0;
+                this.netWeightQuantity = 0;
+                this.positions = 0;
+                this.transportDocumentCodes = new List<string>()
+                {
+                    "02011", "02012", "02013",
+                    "02014", "02015", "02016",
+                    "02017", "02018", "02019",
+                    "02020", "02021", "02022",
+                    "02024", "02025", "02099"
+                };
+                this.awb = new List<(string, string)>();
+                InitializeComponent();
+                XmlHandler(path);
+            }
+            finally
+            {
+                this.grossWeightQuantity = 0;
+                this.netWeightQuantity = 0;
+                this.positions = 0;
+            }
+        }
 
+        void close(IWorkbook wb)
+        {
 
-        //pepper with crutches
-        List<(string, string)> awb = new List<(string,string)>();
+            const string path = @"D:\Test\test.xlsx";
+            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write))
+               wb.Write(fs);
+            wb.Close();
+        }
+        (IWorkbook,ISheet) open()
+        {
+            const string sheetName = "Лист 1";
+            IWorkbook wb = new XSSFWorkbook();
+            ISheet sh = wb.CreateSheet(sheetName);
+            return (wb, sh);
+        }
+
+        IWorkbook save(in string[] row, (IWorkbook,ISheet) book)
+        {
+            if (row == null)
+                MessageBox.Show("row is null");
+            else
+            {
+                IRow currentRow = book.Item2.CreateRow(book.Item2.LastRowNum + 1);
+                const int countColumn = 2;
+                for (int j = 0; j < countColumn; j++)                
+                    currentRow.CreateCell(j).SetCellValue(row[j]);
+
+                
+                return book.Item1;
+            }
+            return default;
+        }
         void add(string[] pair)
         {
-            if(!awb.Contains((pair[0], pair[1])))
-                awb.Add((pair[0], pair[1]));  
+            if (!awb.Contains((pair[0], pair[1])))
+                awb.Add((pair[0], pair[1]));
         }
-        bool checkDocumentCode(string[] number) 
+        bool checkDocumentCode(string[] number)
         {
             if (transportDocumentCodes.Contains(number[1]))
                 return true;
             else return false;
         }
-        void calc(string[] value, ref double result) 
+        void calc(string[] value, ref double result)
         {
-            result +=double.Parse(value[1], System.Globalization.CultureInfo.InvariantCulture);
+            result += double.Parse(value[1], System.Globalization.CultureInfo.InvariantCulture);
         }
         void print(string[] value)
         {
-            if(value != null)
+            if (value != null)
                 block.Text += $"{value[0]} - {value[1]}\n";
         }
         string[] search(string value, XmlNode collection, string name)
@@ -51,6 +107,8 @@ namespace WpfApp3
         {
             XmlDocument document = new XmlDocument();
             document.Load(path);
+
+            (IWorkbook, ISheet) book = open();
 
             XmlElement root = document.DocumentElement;
             foreach (XmlElement item in root)
@@ -68,18 +126,18 @@ namespace WpfApp3
                                         if (general.Name == "ESADout_CUGoodsShipment")
                                         {
                                             {
-                                                print(search("catESAD_cu:TotalPackageNumber", general, "Количество мест"));
-                                                print(search("catESAD_cu:TotalCustCost", general, "Итоговая таможенная стоимоть"));
+                                                save(search("catESAD_cu:TotalPackageNumber", general, "Количество мест"), book);
+                                                save(search("catESAD_cu:TotalCustCost", general, "Итоговая таможенная стоимоть"), book);
                                             }
                                             foreach (XmlNode info in general.ChildNodes)
                                             {
-                                                if (info.Name == "ESADout_CUGoodsLocation") print(search("CustomsOffice", info, "Пост прибытия"));
-                                                if (info.Name == "ESADout_CUConsigment") print(search("catESAD_cu:TransportIdentifier", info, "Транспорт"));
+                                                if (info.Name == "ESADout_CUGoodsLocation") save(search("CustomsOffice", info, "Пост прибытия"), book);
+                                                if (info.Name == "ESADout_CUConsigment") save(search("catESAD_cu:TransportIdentifier", info, "Транспорт"), book);
                                                 if (info.Name == "ESADout_CUMainContractTerms")
                                                 {
-                                                    print(search("catESAD_cu:ContractCurrencyCode", info, "Валюта"));
-                                                    print(search("catESAD_cu:ContractCurrencyRate", info, "Курс"));
-                                                    print(search("ESADout_CUMainContractTerms", info, "Итоговая фактурная стоимоть"));
+                                                    save(search("catESAD_cu:ContractCurrencyCode", info, "Валюта"), book);
+                                                    save(search("catESAD_cu:ContractCurrencyRate", info, "Курс"), book);
+                                                    save(search("ESADout_CUMainContractTerms", info, "Итоговая фактурная стоимоть"), book);
                                                 }
                                                 if (info.Name == "ESADout_CUGoods")
                                                 {
@@ -94,7 +152,7 @@ namespace WpfApp3
                                                                 foreach (XmlNode about in count)
                                                                     if (about.Name == "catESAD_cu:GoodsGroupQuantity")
                                                                         calc(search("cat_ru:GoodsQuantity", about, "Количество товара"), ref positions);
-                                                        
+
                                                     }
                                                     foreach (XmlNode doc in info)
                                                     {
@@ -108,7 +166,7 @@ namespace WpfApp3
                                                                     add(search("cat_ru:PrDocumentNumber", doc, "Номер документа"));
                                                                 }
                                                             }
-                                                            catch (System.ArgumentException exp)
+                                                            catch (System.ArgumentException)
                                                             {
                                                                 /*
                                                                 ╲╲┏━╮╲╲╲╱╱╱╭━┓╱╱
@@ -130,40 +188,15 @@ namespace WpfApp3
                 }
             }
             {
-                print(new string[] { "Общая масса брутто", grossWeightQuantity.ToString() });
-                print(new string[] { "Общая масса нетто", netWeightQuantity.ToString() });
-                print(new string[] { "Всего позиций", positions.ToString() });
+                save(new string[] { "Общая масса брутто", grossWeightQuantity.ToString() }, book);
+                save(new string[] { "Общая масса нетто", netWeightQuantity.ToString() }, book);
+                save(new string[] { "Всего позиций", positions.ToString() }, book);
             }
-
             foreach ((string, string) pair in awb.Distinct())
             {
-                print(new string[] { pair.Item1, pair.Item2});
+                save(new string[] { pair.Item1, pair.Item2 }, book);
             }
-        }
-        public Information(string path)
-        {
-            try
-            {
-                this.grossWeightQuantity = 0;
-                this.netWeightQuantity = 0;
-                this.positions = 0;
-                this.transportDocumentCodes = new List<string>()
-                {
-                    "02011", "02012", "02013", 
-                    "02014", "02015", "02016", 
-                    "02017", "02018", "02019",
-                    "02020", "02021", "02022",
-                    "02024", "02025", "02099"
-                };
-                InitializeComponent();
-                XmlHandler(path);
-            }
-            finally 
-            {
-                this.grossWeightQuantity = 0;
-                this.netWeightQuantity = 0;
-                this.positions = 0;
-            }
+            close(book.Item1);
         }
     }
 }
